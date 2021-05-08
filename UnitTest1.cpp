979ff329f5b5e507a400e0c698bfd59a9568d964
PolinaPo <alexsslave@mail.ru>
2021-05-08 18:19:55 +0700

    add files

---
+++ UnitTest1.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\program.h"
+#include "..\program.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace type_plants;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
